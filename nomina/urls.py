894bdb144f739494ee1ba81ac45e7f3cac818e57
Alexgarciaitz <lagart8@gmail.com>
2022-03-31 19:42:41 -0600

    First deploy_1

--- nomina/urls.py
+++ nomina/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns =[
     path('', views.home, name='home'),
     path('eliminar_empleado/<int:id>', views.eliminar_empleado, name='eliminar_empleado'),
-    path('editar_empleado/<int:id>', views.editar_empleado, name='editar_empleado'),
+    #path('editar_empleado/<int:id>', views.editar_empleado, name='editar_empleado'),
 ]
