558c1b9383f2c85aab53870eb530ccaaf9356c84
Alexgarciaitz <lagart8@gmail.com>
2022-03-31 19:04:18 -0600

    First deploy

--- nomina/models.py
+++ nomina/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Empleado(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField('Nombre', max_length=100)
+    rfc = models.CharField('RFC', max_length=13)
+    curp = models.CharField('CURP', max_length=18)
+    puesto = models.CharField('Puesto', max_length=30)
+    sueldo = models.DecimalField(max_digits=7 ,decimal_places=2)
+    fecha_ingreso = models.DateField()
+    #para imprimir un objeto
+    def __str__(self):
+        return self.nombre
---
+++ nomina/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns =[
+    path('', views.home, name='home'),
+    path('eliminar_empleado/<int:id>', views.eliminar_empleado, name='eliminar_empleado'),
+    path('editar_empleado/<int:id>', views.editar_empleado, name='editar_empleado'),
+]
