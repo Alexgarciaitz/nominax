6bcb764e68bce8dad32d88fe15954c7fa9a4f202
Alexgarciaitz <lagart8@gmail.com>
2022-03-31 19:39:21 -0600

    First deploy_1

--- nominax/settings.py
+++ nominax/settings.py
@@ -40,7 +40,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.whiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'nominax.urls'
