ea666c324895aa9bc9542f71b9e7e87eda683afd
Alexgarciaitz <lagart8@gmail.com>
2022-04-04 17:17:45 -0500

    First deploy_2

--- nominax/settings.py
+++ nominax/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-z0hp(epqimec04i)n8j7q=+g0zu@5z9g*-x@kr-5a%3756(4bn
 # para produccion activarlo o desactivarlo True o False
 DEBUG = False
 # la unica instancia que ejecuta el proyecto
-ALLOWED_HOSTS = ['nominay.herokuapp.com/']
+ALLOWED_HOSTS = ['nominay.herokuapp.com']
 
 
 # Application definition
