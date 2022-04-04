f5bd9e2c20beb7a66dd404116ec645ecc3f9b7a3
Alexgarciaitz <lagart8@gmail.com>
2022-04-04 17:06:40 -0500

    First deploy_2

--- nomina/urls.py
+++ nomina/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.home, name='home'),
+    path('', views.home, name='home.html'),
     path('eliminar_empleado/<int:id>', views.eliminar_empleado, name='eliminar_empleado'),
     #path('editar_empleado/<int:id>', views.editar_empleado, name='editar_empleado'),
 ]
