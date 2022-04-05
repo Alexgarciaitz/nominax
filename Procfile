0d775ebcb9d9e09c5085ae72b4597f961cb19788
Alexgarciaitz <101300613+Alexgarciaitz@users.noreply.github.com>
2022-04-04 19:31:20 -0500

    quitar todo de nuevo

--- Procfile
+++
@@ -1,4 +0,0 @@
-release python manage.py makemigrations --no-input
-release python manage.py migrate --no-input
-
-web: gunicorn nominax.wsgi --log-file -
--- nomina/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns =[
-    path('', views.home, name='home'),
-    path('eliminar_empleado/<int:id>', views.eliminar_empleado, name='eliminar_empleado'),
-    #path('editar_empleado/<int:id>', views.editar_empleado, name='editar_empleado'),
-]
