2a1a6d9d4b26e856ad4a46ce801b5287e0619fb3
Alexgarciaitz <101300613+Alexgarciaitz@users.noreply.github.com>
2022-04-04 19:33:47 -0500

    poner todo de nuevo

---
+++ nomina/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from . models import Empleado
+
+def home(request):
+    empleados = Empleado.objects.all()
+    return render(request, 'home.html', {'empleados': empleados})
+    # return HttpResponse("hola")
+# Create your views here.
+
+def eliminar_empleado(request, id):
+    empleado =Empleado.objects.get(id=id)
+    empleado.delete()
+    return redirect('/')
